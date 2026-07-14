444da1c934266726f0639270ff7ca1525a841432
dephea <lwarayl@hotmail.com>
2026-07-14 21:55:33 +0300

    add mock movies

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -1,6 +1,19 @@
+import {movies} from './mock-movies.json'
+
 const MoviePage = () => {
     return <div>
-        This is movie page
+
+        <h1 className="text-4xl font-bold text-blue-600 mb-6">
+            This is movie page
+        </h1>
+        
+        <div className="space-y-4">
+            {movies.map((movie) => (
+            <div className="p-5 border-2 border-blue-200 hover:border-blue-500"><p key={movie.title}>
+            {movie.title} ({movie.year}) — {movie.genre}
+            </p> </div>
+        ))}
+        </div>
     </div>
 }
 

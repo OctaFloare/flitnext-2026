a7d323eb67868473ffdd0d65fb1c00b18a6f8512
Barna Tudor <53333717+barna-tudor@users.noreply.github.com>
2026-07-21 10:28:43 +0300

    Update middleware.ts
    Co-authored-by: OctaFloare <74454053+OctaFloare@users.noreply.github.com>

--- middleware.ts
+++ middleware.ts
@@ -20,7 +20,7 @@ export async function middleware(req: NextRequest, res: NextResponse) {
 
     const authToken = req.cookies.get('authToken')?.value
     const pathname = req.nextUrl.pathname;
-    let tokenExpired = null;
+    const tokenExpired = authToken ? isTokenExpired(authToken) : null;
     if (authToken) {
         tokenExpired = isTokenExpired(authToken);
     }

da8aca7614c5a7cbb4a8de244b631d596a6595b0
AndreiBcs <andreibichis1@gmail.com>
2026-07-21 10:25:09 +0300

    fix after review

--- app/broken-page/page.tsx
+++ app/broken-page/page.tsx
@@ -1,5 +1,7 @@
 import { BrokenPage as BrokenPageComponent } from "@/components/broken-page"
 
-export default function BrokenPage() {
+export const BrokenPage = () => {
     return <BrokenPageComponent title={"Broken page"} /> 
 }
+
+export default BrokenPage

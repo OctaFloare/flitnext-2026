7e3ed605e75014d3164082a63b6fbd5c04828f63
barna-tudor <tudorbarna00@gmail.com>
2026-07-22 09:49:04 +0300

    initial commit
    # Conflicts:
    #	components/movies/movies-content.tsx
    #	package-lock.json
    #	yarn.lock

--- components/movies/movies-content.tsx
+++ components/movies/movies-content.tsx
@@ -4,6 +4,9 @@ import Image from "next/image"
 import Link from "next/link"
 import { useQuery } from "@tanstack/react-query"
 import * as data from '../../mock.json'
+import {Swiper, SwiperSlide} from 'swiper/react';
+import 'swiper/css';
+
 
 type Movies = typeof data
 
@@ -19,29 +22,36 @@ export const MoviesContent = () => {
 
     console.log(data, error , "this is response")
 
-    return <div>
+    return <Swiper>
         <h1 className="font-bold font-sans text-amber-400 text-4xl mb-10 mt-6">
             Movie Catalog
         </h1>
-        <div className="grid grid-cols-3 gap-5">
+
+        <Swiper>
             {data && data.map((movie) => (!movie.deleted &&
-                <div data-testid="movie-card" className="flex p-5 border-2 border-amber-300 items-center justify-center flex-col" key={movie.id}>
-                    <>
-                        <p className="text-lg m-2">{movie.title}</p><p className="text-gray-300 font-bold">({movie.release_year})</p>
-                    </>
-                    <>
-                        <p>Genres: {movie.genres.join(", ")}</p>
-                        <Link href={`/movies/${movie.id}`}>
-                        <Image 
-                            src={movie.imgUrl} 
-                            alt={`${movie.title}-image`}
-                            width={200}
-                            height={200}
-                            
-                        /></Link>
-                    </>
-                </div>
-            ))}
-        </div>
+                <SwiperSlide key={movie.id}>
+
+                </SwiperSlide>))}
+        </Swiper>
+        {/*<div className="grid grid-cols-3 gap-5">*/}
+        {/*    {data && data.map((movie) => (!movie.deleted &&*/}
+        {/*        <div className="flex p-5 border-2 border-amber-300 items-center justify-center flex-col" key={movie.id}>*/}
+        {/*            <>*/}
+        {/*                <p className="text-lg m-2">{movie.title}</p><p className="text-gray-300 font-bold">({movie.release_year})</p>*/}
+        {/*            </>*/}
+        {/*            <>*/}
+        {/*                <p>Genres: {movie.genres.join(", ")}</p>*/}
+        {/*                <Link href={`/movies/${movie.id}`}>*/}
+        {/*                <Image*/}
+        {/*                    src={movie.imgUrl}*/}
+        {/*                    alt={`${movie.title}-image`}*/}
+        {/*                    width={200}*/}
+        {/*                    height={200}*/}
+
+        {/*                /></Link>*/}
+        {/*            </>*/}
+        {/*        </div>*/}
+        {/*    ))}*/}
+        {/*</div>*/}
     </div>
 }

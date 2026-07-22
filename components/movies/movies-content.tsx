2d7fbb275c5b04ed4ce2b9d30fb93922a468fc0b
barna-tudor <tudorbarna00@gmail.com>
2026-07-22 09:57:54 +0300

    initial layout
    initial layout
    
    fix login page and add 'user:user' and 'admin:admin' users
    
    fix
    
    [Carousel] add styles
    
    [Carousel] Slides per view
    
    make slider elements wider
    
    fix styles
    
    add breakpoints for carousel slidesPerView

--- components/movies/movies-content.tsx
+++ components/movies/movies-content.tsx
@@ -5,8 +5,10 @@ import Link from "next/link"
 import { useQuery } from "@tanstack/react-query"
 import * as data from '../../mock.json'
 import {Swiper, SwiperSlide} from 'swiper/react';
+import { Navigation } from "swiper/modules"
 import 'swiper/css';
-
+import 'swiper/css/navigation';
+import './styles.css';
 
 type Movies = typeof data
 
@@ -22,36 +24,41 @@ export const MoviesContent = () => {
 
     console.log(data, error , "this is response")
 
-    return <Swiper>
+    return <div>
         <h1 className="font-bold font-sans text-amber-400 text-4xl mb-10 mt-6">
             Movie Catalog
         </h1>
 
-        <Swiper>
+        <Swiper navigation={true}
+        modules={[Navigation]}
+        className="mySwiper"
+        slidesPerView={1}
+        breakpoints={{
+            480: { slidesPerView: 2 },
+            768: { slidesPerView: 3 },
+            1024: { slidesPerView: 4 },
+            1280: { slidesPerView: 5 },
+        }}
+        autoHeight= {true}
+        spaceBetween={0}
+        >
             {data && data.map((movie) => (!movie.deleted &&
-                <SwiperSlide key={movie.id}>
+                <SwiperSlide key={movie.id} className="">
+                    <div className="w-63">
+                        <p className="text-lg m-2">{movie.title}</p><p
+                        className="text-gray-300 font-bold">({movie.release_year})</p>
 
+                        <p>Genres: {movie.genres.join(", ")}</p>
+                        <Link href={`/movies/${movie.id}`}>
+                            <Image
+                                src={movie.imgUrl}
+                                alt={`${movie.title}-image`}
+                                width={200}
+                                height={200}
+
+                            /></Link>
+                    </div>
                 </SwiperSlide>))}
         </Swiper>
-        {/*<div className="grid grid-cols-3 gap-5">*/}
-        {/*    {data && data.map((movie) => (!movie.deleted &&*/}
-        {/*        <div className="flex p-5 border-2 border-amber-300 items-center justify-center flex-col" key={movie.id}>*/}
-        {/*            <>*/}
-        {/*                <p className="text-lg m-2">{movie.title}</p><p className="text-gray-300 font-bold">({movie.release_year})</p>*/}
-        {/*            </>*/}
-        {/*            <>*/}
-        {/*                <p>Genres: {movie.genres.join(", ")}</p>*/}
-        {/*                <Link href={`/movies/${movie.id}`}>*/}
-        {/*                <Image*/}
-        {/*                    src={movie.imgUrl}*/}
-        {/*                    alt={`${movie.title}-image`}*/}
-        {/*                    width={200}*/}
-        {/*                    height={200}*/}
-
-        {/*                /></Link>*/}
-        {/*            </>*/}
-        {/*        </div>*/}
-        {/*    ))}*/}
-        {/*</div>*/}
     </div>
 }

2062cd1f0953cb922050f722650aa49b347ae8ad
barna-tudor <tudorbarna00@gmail.com>
2026-07-23 11:03:30 +0300

    update movie content page

--- components/movie/movie-content.tsx
+++ components/movie/movie-content.tsx
@@ -1,15 +1,15 @@
 'use client'
 
-import { useQuery } from "@tanstack/react-query"
+import {useQuery} from "@tanstack/react-query"
 import * as moviesData from '../../mock.json'
-import { useDeleteMovie } from "./hooks/useDeleteMovie"
+import {useDeleteMovie} from "./hooks/useDeleteMovie"
 
 type Movie = typeof moviesData[0]
 
-export const MovieContent = ({ movieId } : {
+export const MovieContent = ({movieId}: {
     movieId: number,
 }) => {
-    const { data, error } = useQuery<Movie>({
+    const {data, error} = useQuery<Movie>({
         queryKey: ['movie', `${movieId}`],
         queryFn: async () => {
             const result = await fetch(`/api/movie/${movieId}`, {
@@ -20,27 +20,30 @@ export const MovieContent = ({ movieId } : {
         }
     })
 
-    const { mutate, data: deleteData } = useDeleteMovie({ movieId })
+    const {mutate, data: deleteData} = useDeleteMovie({movieId})
 
     const onDeleteClick = () => {
         mutate()
     }
 
     return <div>
-        <div>
-      <h1 className="font-bold font-sans text-amber-400 text-4xl mb-10 mt-6">
-          Movie id: {movieId}
-      </h1>
-      <button className="bg-red-500 p-2" onClick={onDeleteClick}>Delete Movie</button>
-      {deleteData && <div>{deleteData.message}</div>}
-      </div>
-      <div className="grid grid-cols-3 gap-5">
-          {data && <>{data.title}
-          <video width="320" height="240" controls>
-            <source src={`${data.sourceUrl}`}></source>
-            Your browser does not support the video tag.
-          </video>
-          </>}
-      </div>
-  </div>
+        <div className="items-center justify-center flex">
+            {data &&
+                <div className="flex items-center justify-center gap-2">
+                    <h1 className="font-bold font-sans text-amber-400 text-4xl mb-10 mt-6 "> {data.title} </h1>
+                    <h1 className="font-bold font-sans text-amber-200 text-4xl mb-10 mt-6 "> ({data.release_year})</h1>
+                </div>}
+        </div>
+        <div className="flex items-center justify-center w-full">
+            {data &&
+                <div>
+                    <video width="1080" height="720" controls>
+                        <source src={`${data.sourceUrl}`}></source>
+                        Your browser does not support the video tag.
+                    </video>
+                </div>}
+        </div>
+        <button className="bg-red-500 p-2" onClick={onDeleteClick}>Delete Movie</button>
+        {deleteData && <div>{deleteData.message}</div>}
+    </div>
 }
